d31694ceabe0dcf1449137e71c73264a4d2e24f2
= <=>
2023-10-13 17:37:58 -0400

    added validation example

--- utils/ALFA-K.R
+++ utils/ALFA-K.R
@@ -282,3 +282,26 @@ getf <- function(pk,fobj){
   d <- apply(fobj$peaks,1,function(pki) sqrt(sum((pk-pki)^2)))
   sum(sin(d/fobj$wavelength)*fobj$scale)
 }
+
+#leave one out cross validation procedure
+optim_loo <- function(i,xx,xo){
+  xo <- xo[,c("f_est","u0","ll","n","ntp")]
+  xi <- rownames(xo)[i]
+  xistr <- xi
+  print(xi)
+  xi <-as.numeric(unlist(strsplit(xi,split="[.]"))) 
+  xtrial <- xo
+  xtrial <- xtrial[!rownames(xtrial)==xistr,]
+  x2 <- tryCatch(wrap_neighbor_fitness(xx,xtrial),
+                 error=function(e) return(NULL))
+  if(is.null(x2)) return(data.frame(pred=NaN,row.names=xistr))
+  x2 <- x2[!rownames(x2)==xistr,]
+  x2 <- rbind(xtrial,x2)
+  xmat <- do.call(rbind,lapply(rownames(x2), function(i){
+    as.numeric(unlist(strsplit(i,split="[.]")))
+  }))
+  y <- x2$f_est
+  fit <- Krig(xmat,y,m=1)
+  pred <- predict(fit,matrix(xi,nrow=1))
+  data.frame(pred=pred,row.names = xistr)
+}
