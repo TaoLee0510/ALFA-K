003f7c873900d8f6d5fa6da7cfa8a8f84ee33099
= <=>
2024-12-18 14:14:50 -0500

    improve aggregation functions

---
+++ utils/aggregate_sweep_results.R
@@ -0,0 +1,16 @@
+## various functions for aggregating ABM sweep output.
+
+## this function is used when we have data structure
+##  inDir
+##    subdir
+##      target
+## where target is a data frame.
+aggregate_fit_summaries <- function(summaryName="fit_summaries.Rds",inDir="data/main/",outPath="data/proc/summaries/"){
+  ## make sure directory exists
+  dir.create(outPath,recursive = T,showWarnings = F)
+  ff <- list.files(inDir)
+  ff <- paste0(inDir,ff,summaryName)
+  ff <- ff[file.exists(ff)]
+  x <- lapply(ff,readRDS)
+  return(x)
+}
