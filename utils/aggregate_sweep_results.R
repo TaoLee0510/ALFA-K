f766693e95bd3bf03197f44c7c822a0eb98e6e9c
= <=>
2025-01-07 14:34:44 -0500

    debug sweep proc functions

--- utils/aggregate_sweep_results.R
+++ utils/aggregate_sweep_results.R
@@ -180,7 +180,7 @@ wasserstein_comps <- function(subdir_1="train",subdir_2="train",range_1 = 2000,
   
     
   # Export variables to the cluster
-  clusterExport(cl, varlist = c("eval_range", "delta_t", "df","wasserstein_matrix",
+  clusterExport(cl, varlist = c("range_1","range_2", "delta_t", "df","wasserstein_matrix",
                                 "get_eval_times"), envir = environment())
   
 
@@ -197,7 +197,9 @@ wasserstein_comps <- function(subdir_1="train",subdir_2="train",range_1 = 2000,
       return(NA)
     })
   })
-  list(res=res,df=df)
+  output <- list(res=res,df=df)
+  saveRDS(output,outPath)
+  return(output)
 }
 
 compute_wasserstein_distances <- function(subdir_1="train", subdir_2="test", 
